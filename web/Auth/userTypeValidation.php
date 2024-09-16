78c1d31267a6e4d500385e01c6ae8b31b0472def
CASEMIRO SENEME JUNIOR <casemiro.seneme@fatecararas.com>
2024-09-16 20:24:24 -0300

    Refactor user type validation logic and improve session handling

--- web/Auth/userTypeValidation.php
+++ web/Auth/userTypeValidation.php
@@ -1,17 +1,16 @@
 <?php
 session_start();
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && $_SESSION['loggedin'] !== TRUE) {
     if ($_POST['username'] == 'coordenacao' and $_POST['password'] == 'coordenacao' || $_SESSION['username'] == 'coordenacao') {
         $_SESSION['loggedin'] = TRUE;
         $_SESSION['username'] = 'coordenacao';
         header("location: ./pages/menuCoord.php");
-    } else if ($_POST['username'] == 'tecnicos' and $_POST['password'] == 'tecnicos' || $_SESSION['username'] == 'tecnicos' ) {
+    } else if ($_POST['username'] == 'tecnicos' and $_POST['password'] == 'tecnicos' || $_SESSION['username'] == 'tecnicos') {
         $_SESSION['loggedin'] = TRUE;
         $_SESSION['username'] = 'tecnicos';
         header("location: ./pages/menuTec.php");
-    } else {
-        echo "<script>alert('Usuário ou senha incorretos!')</script>";
-        if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+    } else if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) { {
+            echo "<script>alert('Usuário ou senha incorretos!')</script>";
             header("location: /prova/FATEC_DES_WEB2_2024_Avaliacao1/web/index.php");
             exit;
         }
